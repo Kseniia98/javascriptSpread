bec65e1ad98d35bb70a98d406500f701e7c82b48
Kseniia <kseniia.plakhotnia@gmail.com>
2022-02-11 16:58:28 +0200

    Add examples

---
+++ index.js
@@ -0,0 +1,65 @@
+"use strict"
+
+// const object = {
+//   countryName: "Chech Republic",
+//   population: 35000000,
+//   areaSqKM: 345000, //площадь в квадратных км
+//   helloPhrase: "Dobry Dzen",
+//   nationalities: ['ukrainian', 'poles']
+// }
+
+// 2.
+// const {countryName, helloPhrase} = object;
+// alert(`${countryName} ${helloPhrase}`);
+
+// 3.
+// const {population, areaSqKM} = object;
+// const a = population / areaSqKM;
+// console.log(a);
+
+// 4.
+
+// const { nationalities: [first] } = object;
+// console.log(first)
+
+//массивы
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// const cloneArr = [...arr];
+
+
+// const a = "тест строки как массива";
+// const b = [...a];
+// console.log(b);
+
+//Обьекты
+
+// const obj1 = {
+//   id: 76,
+//   firstname: 'ghf', 
+//   lastname: 'gfhf', 
+//   age: 56,
+// }
+
+// const obj2 = {
+//   tel: 54747,
+//   email: 'yghhy',
+// }
+
+// const obj3 = {
+//   ...obj1,
+//   ...obj2,
+// }
+// console.log(obj3);
+  
+//Оператор Rest:
+1.
+// function funcRest (...values){
+//   console.log(values)
+// }
+// funcRest(1,2,3,4,5,6,7,8)
+
+2.
+function func(...arg) {
+  
+}
